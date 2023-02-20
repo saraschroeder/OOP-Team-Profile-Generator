6a324a61e4166263d3085ee8b0ce4437d11175e7
saraschroeder <sara.schroeder00@gmail.com>
2023-02-20 16:12:50 -0600

    added inital code to employee.test.js

---
+++ tests/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Madison', 1, 'madison@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('gets employee name', () => {
+    const employee = new Employee('Madison', 1, 'madison@gmail.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employee ID', () => {
+    const employee = new Employee('Madison', 1, 'madison@gmail.com');
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Madison', 1, 'madison@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test('gets role of employee', () => {
+    const employee = new Employee('Madison', 1, 'madison@gmail.com');
+});
