64ce782c367697eb8342a150d881ff1ff87af843
saraschroeder <sara.schroeder00@gmail.com>
2023-03-04 14:47:35 -0600

    additional code to index.js

--- index.js
+++ index.js
@@ -53,7 +53,134 @@ const addManager = () => {
             }
         },
         {
-            
+            type: 'input',
+            name: 'officeNumber',
+            message: "please enter the manager's office number",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("Please enter an office number")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         }
     ])
-}
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        teamArray.push(manager);
+        console.log(manager);
+    })
+};
+
+const addEmployee = () => {
+    console.log(`
+    =================
+    Adding employees to the team
+    =================
+    `);
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose employee's role",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the employee's ID.",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("Please enter the employee's ID.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the employee's email",
+            validate: email => {
+                valid = /^\w+([\.-]?\w=)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email.')
+                    return false;
+                }
+            }
+        },
+         {
+                type: 'input',
+                name: 'github',
+                message: "Please enter the employee's github username",
+                when: (input) => input.role === "Engineer",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log ("Please enter the employee's github username.")
+                    }
+                }
+          },
+          {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the intern's school.")
+                }
+            }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddEmployee',
+        message: "Would you like to add more team members?",
+        default: false
+     }
+  ])
+  .then(employeeData => {
+    let {name, id, email, role, github, school, confirmAddEmployee} = employeeData;
+    let employee;
+
+    if(role === "Engineer") {
+        employee = new Engineer (name, id, email, github);
+        console.log(employee);
+    } else if (role === "Intern") {
+        employee = new Intern (name, id, email, school);
+        console.log(employee);
+    }
+
+teamArray.push(employee);
+
+if (confirmAddEmployee) {
+    return addEmployee(teamArray);
+     } else {
+    return teamArray;
+     }
+
+    })
+};
